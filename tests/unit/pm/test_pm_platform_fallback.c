01b15a6b867a19873cde36a4df459b409c33e24c
ZeroZap <zerozap2020@gmail.com>
2026-06-25 19:38:51 +0800

    test: migrate pm platform fallback tests to unity

--- tests/unit/pm/test_pm_platform_fallback.c
+++ tests/unit/pm/test_pm_platform_fallback.c
@@ -1,6 +1,6 @@
-#include <assert.h>
+#include "unity.h"
+
 #include <stdint.h>
-#include <stdio.h>
 #include <string.h>
 
 #include "xy_pm.h"
@@ -8,40 +8,48 @@
 void xy_pm_platform_set_fallback_tick(uint32_t tick);
 int xy_pm_platform_get_charger_enable_level(void);
 
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
 static void test_fallback_platform_identity(void)
 {
-    assert(strcmp(xy_pm_get_platform_name(), "Unknown") == 0);
-    assert(!xy_pm_is_platform(XY_PLATFORM_ID_PC));
-    assert(!xy_pm_is_platform(XY_PLATFORM_ID_STM32));
-    assert(!xy_pm_is_platform(XY_PLATFORM_ID_WCH));
-    assert(!xy_pm_is_platform(XY_PLATFORM_ID_HC32));
-    assert(!xy_pm_is_platform(XY_PLATFORM_ID_UNKNOWN));
+    TEST_ASSERT_EQUAL_STRING("Unknown", xy_pm_get_platform_name());
+    TEST_ASSERT_FALSE(xy_pm_is_platform(XY_PLATFORM_ID_PC));
+    TEST_ASSERT_FALSE(xy_pm_is_platform(XY_PLATFORM_ID_STM32));
+    TEST_ASSERT_FALSE(xy_pm_is_platform(XY_PLATFORM_ID_WCH));
+    TEST_ASSERT_FALSE(xy_pm_is_platform(XY_PLATFORM_ID_HC32));
+    TEST_ASSERT_FALSE(xy_pm_is_platform(XY_PLATFORM_ID_UNKNOWN));
 }
 
 static void test_fallback_tick_has_no_read_side_effect(void)
 {
     xy_pm_platform_set_fallback_tick(1234U);
-    assert(xy_pm_tick_get() == 1234U);
-    assert(xy_pm_tick_get() == 1234U);
+    TEST_ASSERT_EQUAL_UINT32(1234U, xy_pm_tick_get());
+    TEST_ASSERT_EQUAL_UINT32(1234U, xy_pm_tick_get());
 
     xy_pm_platform_set_fallback_tick(4321U);
-    assert(xy_pm_tick_get() == 4321U);
+    TEST_ASSERT_EQUAL_UINT32(4321U, xy_pm_tick_get());
 }
 
 static void test_fallback_charger_hooks_are_safe_noops(void)
 {
-    assert(xy_charger_hw_init() == XY_PM_OK);
-    assert(xy_charger_hw_enable(1) == XY_PM_OK);
-    assert(xy_pm_platform_get_charger_enable_level() == 1);
-    assert(xy_charger_hw_disable() == XY_PM_OK);
-    assert(xy_pm_platform_get_charger_enable_level() == 0);
+    TEST_ASSERT_EQUAL(XY_PM_OK, xy_charger_hw_init());
+    TEST_ASSERT_EQUAL(XY_PM_OK, xy_charger_hw_enable(1));
+    TEST_ASSERT_EQUAL_INT(1, xy_pm_platform_get_charger_enable_level());
+    TEST_ASSERT_EQUAL(XY_PM_OK, xy_charger_hw_disable());
+    TEST_ASSERT_EQUAL_INT(0, xy_pm_platform_get_charger_enable_level());
 }
 
 int main(void)
 {
-    test_fallback_platform_identity();
-    test_fallback_tick_has_no_read_side_effect();
-    test_fallback_charger_hooks_are_safe_noops();
-    puts("PM platform fallback tests passed");
-    return 0;
+    UNITY_BEGIN();
+    RUN_TEST(test_fallback_platform_identity);
+    RUN_TEST(test_fallback_tick_has_no_read_side_effect);
+    RUN_TEST(test_fallback_charger_hooks_are_safe_noops);
+    return UNITY_END();
 }
