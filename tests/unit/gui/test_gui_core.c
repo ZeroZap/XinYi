b406c3f795372fb9888851436b2aae9354fcbec4
ZeroZap <zerozap2020@gmail.com>
2026-06-25 20:36:51 +0800

    test: migrate gui core tests to unity

--- tests/unit/gui/test_gui_core.c
+++ tests/unit/gui/test_gui_core.c
@@ -1,8 +1,8 @@
-#include <assert.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
 
+#include "unity.h"
 #include "xy_gui.h"
 
 static int g_init_count;
@@ -17,6 +17,14 @@ static int16_t g_last_y;
 static int16_t g_last_w;
 static int16_t g_last_h;
 static uint16_t g_last_color;
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
 
 static void reset_mock(void)
 {
@@ -118,30 +126,32 @@ static void test_lifecycle_clear_flush_and_bounds(void)
     xy_gui_disp_drv_t drv = mock_driver();
 
     reset_mock();
-    assert(xy_gui_init(NULL, 64, 32, &drv) == XY_GUI_INVALID_PARAM);
-    assert(xy_gui_init(&gui, 64, 32, NULL) == XY_GUI_INVALID_PARAM);
-    assert(xy_gui_init(&gui, 64, 32, &drv) == XY_GUI_OK);
-    assert(gui.initialized);
-    assert(gui.width == 64);
-    assert(gui.height == 32);
-    assert(g_init_count == 1);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_INVALID_PARAM, xy_gui_init(NULL, 64, 32, &drv));
+    TEST_ASSERT_EQUAL_INT(XY_GUI_INVALID_PARAM, xy_gui_init(&gui, 64, 32, NULL));
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_init(&gui, 64, 32, &drv));
+    TEST_ASSERT_TRUE(gui.initialized);
+    TEST_ASSERT_EQUAL_INT16(64, gui.width);
+    TEST_ASSERT_EQUAL_INT16(32, gui.height);
+    TEST_ASSERT_EQUAL_INT(1, g_init_count);
 
-    assert(xy_gui_clear(&gui, GUI_COLOR_BLUE) == XY_GUI_OK);
-    assert(gui.bg_color == GUI_COLOR_BLUE);
-    assert(g_fill_count == 1);
-    assert(g_last_w == 64 && g_last_h == 32);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_clear(&gui, GUI_COLOR_BLUE));
+    TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_BLUE, gui.bg_color);
+    TEST_ASSERT_EQUAL_INT(1, g_fill_count);
+    TEST_ASSERT_EQUAL_INT16(64, g_last_w);
+    TEST_ASSERT_EQUAL_INT16(32, g_last_h);
 
-    assert(xy_gui_draw_pixel(&gui, 63, 31, GUI_COLOR_RED) == XY_GUI_OK);
-    assert(g_pixel_count == 1);
-    assert(g_last_x == 63 && g_last_y == 31);
-    assert(xy_gui_draw_pixel(&gui, 64, 0, GUI_COLOR_RED) == XY_GUI_INVALID_PARAM);
-    assert(xy_gui_draw_pixel(&gui, -1, 0, GUI_COLOR_RED) == XY_GUI_INVALID_PARAM);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_draw_pixel(&gui, 63, 31, GUI_COLOR_RED));
+    TEST_ASSERT_EQUAL_INT(1, g_pixel_count);
+    TEST_ASSERT_EQUAL_INT16(63, g_last_x);
+    TEST_ASSERT_EQUAL_INT16(31, g_last_y);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_INVALID_PARAM, xy_gui_draw_pixel(&gui, 64, 0, GUI_COLOR_RED));
+    TEST_ASSERT_EQUAL_INT(XY_GUI_INVALID_PARAM, xy_gui_draw_pixel(&gui, -1, 0, GUI_COLOR_RED));
 
-    assert(xy_gui_flush(&gui) == XY_GUI_OK);
-    assert(g_flush_count == 1);
-    assert(xy_gui_deinit(&gui) == XY_GUI_OK);
-    assert(!gui.initialized);
-    assert(xy_gui_clear(&gui, GUI_COLOR_BLACK) == XY_GUI_INVALID_PARAM);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_flush(&gui));
+    TEST_ASSERT_EQUAL_INT(1, g_flush_count);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_deinit(&gui));
+    TEST_ASSERT_FALSE(gui.initialized);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_INVALID_PARAM, xy_gui_clear(&gui, GUI_COLOR_BLACK));
 }
 
 static void test_drawing_and_string_callbacks(void)
@@ -150,18 +160,21 @@ static void test_drawing_and_string_callbacks(void)
     xy_gui_disp_drv_t drv = mock_driver();
 
     reset_mock();
-    assert(xy_gui_init(&gui, 64, 32, &drv) == XY_GUI_OK);
-    assert(xy_gui_draw_line(&gui, 0, 0, 3, 0, GUI_COLOR_GREEN) == XY_GUI_OK);
-    assert(g_pixel_count == 4);
-    assert(xy_gui_draw_rect(&gui, 1, 2, 4, 5, GUI_COLOR_WHITE) == XY_GUI_OK);
-    assert(g_pixel_count > 4);
-    assert(xy_gui_fill_rect(&gui, 2, 3, 5, 6, GUI_COLOR_CYAN) == XY_GUI_OK);
-    assert(g_fill_count == 1);
-    assert(g_last_x == 2 && g_last_y == 3 && g_last_w == 5 && g_last_h == 6);
-    assert(xy_gui_draw_char(&gui, 0, 0, 'A', GUI_COLOR_WHITE) == XY_GUI_OK);
-    assert(g_pixel_count > 4);
-    assert(xy_gui_draw_string(&gui, 0, 0, "Hi", GUI_COLOR_WHITE) == XY_GUI_OK);
-    assert(xy_gui_draw_string(&gui, 0, 0, NULL, GUI_COLOR_WHITE) == XY_GUI_INVALID_PARAM);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_init(&gui, 64, 32, &drv));
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_draw_line(&gui, 0, 0, 3, 0, GUI_COLOR_GREEN));
+    TEST_ASSERT_EQUAL_INT(4, g_pixel_count);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_draw_rect(&gui, 1, 2, 4, 5, GUI_COLOR_WHITE));
+    TEST_ASSERT_GREATER_THAN_INT(4, g_pixel_count);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_fill_rect(&gui, 2, 3, 5, 6, GUI_COLOR_CYAN));
+    TEST_ASSERT_EQUAL_INT(1, g_fill_count);
+    TEST_ASSERT_EQUAL_INT16(2, g_last_x);
+    TEST_ASSERT_EQUAL_INT16(3, g_last_y);
+    TEST_ASSERT_EQUAL_INT16(5, g_last_w);
+    TEST_ASSERT_EQUAL_INT16(6, g_last_h);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_draw_char(&gui, 0, 0, 'A', GUI_COLOR_WHITE));
+    TEST_ASSERT_GREATER_THAN_INT(4, g_pixel_count);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_draw_string(&gui, 0, 0, "Hi", GUI_COLOR_WHITE));
+    TEST_ASSERT_EQUAL_INT(XY_GUI_INVALID_PARAM, xy_gui_draw_string(&gui, 0, 0, NULL, GUI_COLOR_WHITE));
 }
 
 static void test_object_lifecycle_properties_and_redraw(void)
@@ -174,44 +187,46 @@ static void test_object_lifecycle_properties_and_redraw(void)
     xy_gui_obj_t external = {0};
 
     reset_mock();
-    assert(xy_gui_init(&gui, 80, 40, &drv) == XY_GUI_OK);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_init(&gui, 80, 40, &drv));
     label = xy_gui_obj_create(&gui, XY_GUI_OBJ_LABEL, &rect);
-    assert(label != NULL);
-    assert(gui.obj_count == 1);
-    assert(label->visible);
-    assert(label->enabled);
-    assert(label->fg_color.color == GUI_COLOR_BLACK);
-    assert(label->bg_color.color == GUI_COLOR_WHITE);
+    TEST_ASSERT_NOT_NULL(label);
+    TEST_ASSERT_EQUAL_INT(1, gui.obj_count);
+    TEST_ASSERT_TRUE(label->visible);
+    TEST_ASSERT_TRUE(label->enabled);
+    TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_BLACK, label->fg_color.color);
+    TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_WHITE, label->bg_color.color);
 
-    assert(xy_gui_obj_set_text(&gui, label, "OK") == XY_GUI_OK);
-    assert(label->text != NULL && strcmp(label->text, "OK") == 0);
-    assert(xy_gui_obj_set_color(&gui, label, GUI_COLOR_RED, GUI_COLOR_BLUE) == XY_GUI_OK);
-    assert(label->fg_color.color == GUI_COLOR_RED);
-    assert(label->bg_color.color == GUI_COLOR_BLUE);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_obj_set_text(&gui, label, "OK"));
+    TEST_ASSERT_NOT_NULL(label->text);
+    TEST_ASSERT_EQUAL_STRING("OK", label->text);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_obj_set_color(&gui, label, GUI_COLOR_RED, GUI_COLOR_BLUE));
+    TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_RED, label->fg_color.color);
+    TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_BLUE, label->bg_color.color);
 
-    assert(xy_gui_obj_set_visible(&gui, label, false) == XY_GUI_INVALID_PARAM);
-    assert(!label->visible);
-    assert(xy_gui_obj_redraw(&gui, label) == XY_GUI_INVALID_PARAM);
-    assert(xy_gui_obj_set_visible(&gui, label, true) == XY_GUI_OK);
-    assert(label->visible);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_INVALID_PARAM, xy_gui_obj_set_visible(&gui, label, false));
+    TEST_ASSERT_FALSE(label->visible);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_INVALID_PARAM, xy_gui_obj_redraw(&gui, label));
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_obj_set_visible(&gui, label, true));
+    TEST_ASSERT_TRUE(label->visible);
 
     button = xy_gui_obj_create(&gui, XY_GUI_OBJ_BUTTON, &rect);
-    assert(button != NULL);
-    assert(gui.obj_count == 2);
-    assert(xy_gui_obj_set_text(&gui, button, "B") == XY_GUI_OK);
-    assert(g_fill_count >= 1);
+    TEST_ASSERT_NOT_NULL(button);
+    TEST_ASSERT_EQUAL_INT(2, gui.obj_count);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_obj_set_text(&gui, button, "B"));
+    TEST_ASSERT_GREATER_OR_EQUAL_INT(1, g_fill_count);
 
-    assert(xy_gui_obj_delete(&gui, &external) == XY_GUI_NOT_FOUND);
-    assert(xy_gui_obj_delete(&gui, label) == XY_GUI_OK);
-    assert(gui.obj_count == 1);
-    assert(gui.objects[0].type == XY_GUI_OBJ_BUTTON);
-    assert(xy_gui_obj_delete(NULL, button) == XY_GUI_INVALID_PARAM);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_NOT_FOUND, xy_gui_obj_delete(&gui, &external));
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_obj_delete(&gui, label));
+    TEST_ASSERT_EQUAL_INT(1, gui.obj_count);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OBJ_BUTTON, gui.objects[0].type);
+    TEST_ASSERT_EQUAL_INT(XY_GUI_INVALID_PARAM, xy_gui_obj_delete(NULL, button));
 }
 
 int main(void)
 {
-    test_lifecycle_clear_flush_and_bounds();
-    test_drawing_and_string_callbacks();
-    test_object_lifecycle_properties_and_redraw();
-    return 0;
+    UNITY_BEGIN();
+    RUN_TEST(test_lifecycle_clear_flush_and_bounds);
+    RUN_TEST(test_drawing_and_string_callbacks);
+    RUN_TEST(test_object_lifecycle_properties_and_redraw);
+    return UNITY_END();
 }
