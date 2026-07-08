604233cc864ca32617150833c74559f028df1688
ZeroZap <zerozap2020@gmail.com>
2026-07-08 08:27:14 +0800

    test(unit): introduce fff for max17043 fakes

--- tests/unit/fuel_gauge/test_fg_max17043.c
+++ tests/unit/fuel_gauge/test_fg_max17043.c
@@ -1,4 +1,5 @@
 #include "unity.h"
+#include "fff.h"
 #include "xy_fuel_gauge.h"
 #include "xy_sensor_device.h"
 
@@ -6,6 +7,8 @@
 #include <string.h>
 
 int xy_fuel_gauge_max17043_register(void *i2c_handle, uint8_t addr);
+
+DEFINE_FFF_GLOBALS;
 
 #define MAX17043_ADDR       0x36
 #define REG_VCELL           0x02
@@ -18,16 +21,21 @@ static uint16_t fake_regs[256];
 static xy_sensor_bus_t last_bus;
 static uint8_t last_write_reg;
 static uint16_t last_write_value;
-static int read16_count;
-static int write16_count;
-static uint32_t fake_tick;
 
-uint32_t xy_os_tick_get(void)
-{
-    return fake_tick;
-}
+FAKE_VALUE_FUNC(uint32_t, xy_os_tick_get)
+FAKE_VOID_FUNC(xy_sensor_bus_config_i2c, xy_sensor_bus_t *, void *, uint8_t)
+FAKE_VALUE_FUNC(int, xy_sensor_i2c_read_reg16, xy_sensor_bus_t *, uint8_t, uint16_t *)
+FAKE_VALUE_FUNC(int, xy_sensor_i2c_write_reg16, xy_sensor_bus_t *, uint8_t, uint16_t)
+FAKE_VALUE_FUNC(int, xy_sensor_i2c_read, xy_sensor_bus_t *, uint8_t, uint8_t *, uint16_t)
+FAKE_VALUE_FUNC(int, xy_sensor_i2c_write, xy_sensor_bus_t *, uint8_t, const uint8_t *, uint16_t)
+FAKE_VALUE_FUNC(int, xy_sensor_i2c_read_reg, xy_sensor_bus_t *, uint8_t, uint8_t *)
+FAKE_VALUE_FUNC(int, xy_sensor_i2c_write_reg, xy_sensor_bus_t *, uint8_t, uint8_t)
+FAKE_VALUE_FUNC(int, xy_sensor_spi_read, xy_sensor_bus_t *, uint8_t, uint8_t *, uint16_t)
+FAKE_VALUE_FUNC(int, xy_sensor_spi_write, xy_sensor_bus_t *, uint8_t, const uint8_t *, uint16_t)
+FAKE_VALUE_FUNC(int, xy_sensor_check_device_id, xy_sensor_bus_t *, uint8_t, uint8_t)
+FAKE_VOID_FUNC(xy_sensor_bus_config_spi, xy_sensor_bus_t *, void *, uint8_t)
 
-void xy_sensor_bus_config_i2c(xy_sensor_bus_t *bus, void *handle, uint8_t address)
+static void xy_sensor_bus_config_i2c_impl(xy_sensor_bus_t *bus, void *handle, uint8_t address)
 {
     TEST_ASSERT_NOT_NULL(bus);
     bus->type = XY_SENSOR_BUS_I2C;
@@ -37,18 +45,17 @@ void xy_sensor_bus_config_i2c(xy_sensor_bus_t *bus, void *handle, uint8_t addres
     last_bus = *bus;
 }
 
-int xy_sensor_i2c_read_reg16(xy_sensor_bus_t *bus, uint8_t reg, uint16_t *value)
+static int xy_sensor_i2c_read_reg16_impl(xy_sensor_bus_t *bus, uint8_t reg, uint16_t *value)
 {
     TEST_ASSERT_NOT_NULL(bus);
     TEST_ASSERT_EQUAL(XY_SENSOR_BUS_I2C, bus->type);
     TEST_ASSERT_NOT_NULL(value);
 
     *value = fake_regs[reg];
-    read16_count++;
     return 0;
 }
 
-int xy_sensor_i2c_write_reg16(xy_sensor_bus_t *bus, uint8_t reg, uint16_t value)
+static int xy_sensor_i2c_write_reg16_impl(xy_sensor_bus_t *bus, uint8_t reg, uint16_t value)
 {
     TEST_ASSERT_NOT_NULL(bus);
     TEST_ASSERT_EQUAL(XY_SENSOR_BUS_I2C, bus->type);
@@ -56,75 +63,36 @@ int xy_sensor_i2c_write_reg16(xy_sensor_bus_t *bus, uint8_t reg, uint16_t value)
     fake_regs[reg] = value;
     last_write_reg = reg;
     last_write_value = value;
-    write16_count++;
     return 0;
 }
 
-int xy_sensor_i2c_read(xy_sensor_bus_t *bus, uint8_t reg, uint8_t *data, uint16_t len)
+static void reset_sensor_fakes(void)
 {
-    (void)bus;
-    (void)reg;
-    (void)data;
-    (void)len;
-    return -1;
-}
+    RESET_FAKE(xy_os_tick_get);
+    RESET_FAKE(xy_sensor_bus_config_i2c);
+    RESET_FAKE(xy_sensor_i2c_read_reg16);
+    RESET_FAKE(xy_sensor_i2c_write_reg16);
+    RESET_FAKE(xy_sensor_i2c_read);
+    RESET_FAKE(xy_sensor_i2c_write);
+    RESET_FAKE(xy_sensor_i2c_read_reg);
+    RESET_FAKE(xy_sensor_i2c_write_reg);
+    RESET_FAKE(xy_sensor_spi_read);
+    RESET_FAKE(xy_sensor_spi_write);
+    RESET_FAKE(xy_sensor_check_device_id);
+    RESET_FAKE(xy_sensor_bus_config_spi);
+    FFF_RESET_HISTORY();
 
-int xy_sensor_i2c_write(xy_sensor_bus_t *bus, uint8_t reg, const uint8_t *data, uint16_t len)
-{
-    (void)bus;
-    (void)reg;
-    (void)data;
-    (void)len;
-    return -1;
-}
-
-int xy_sensor_i2c_read_reg(xy_sensor_bus_t *bus, uint8_t reg, uint8_t *value)
-{
-    (void)bus;
-    (void)reg;
-    (void)value;
-    return -1;
-}
-
-int xy_sensor_i2c_write_reg(xy_sensor_bus_t *bus, uint8_t reg, uint8_t value)
-{
-    (void)bus;
-    (void)reg;
-    (void)value;
-    return -1;
-}
-
-int xy_sensor_spi_read(xy_sensor_bus_t *bus, uint8_t reg, uint8_t *data, uint16_t len)
-{
-    (void)bus;
-    (void)reg;
-    (void)data;
-    (void)len;
-    return -1;
-}
-
-int xy_sensor_spi_write(xy_sensor_bus_t *bus, uint8_t reg, const uint8_t *data, uint16_t len)
-{
-    (void)bus;
-    (void)reg;
-    (void)data;
-    (void)len;
-    return -1;
-}
-
-int xy_sensor_check_device_id(xy_sensor_bus_t *bus, uint8_t id_reg, uint8_t expected_id)
-{
-    (void)bus;
-    (void)id_reg;
-    (void)expected_id;
-    return -1;
-}
-
-void xy_sensor_bus_config_spi(xy_sensor_bus_t *bus, void *handle, uint8_t cs_pin)
-{
-    (void)bus;
-    (void)handle;
-    (void)cs_pin;
+    xy_os_tick_get_fake.return_val = 1000;
+    xy_sensor_bus_config_i2c_fake.custom_fake = xy_sensor_bus_config_i2c_impl;
+    xy_sensor_i2c_read_reg16_fake.custom_fake = xy_sensor_i2c_read_reg16_impl;
+    xy_sensor_i2c_write_reg16_fake.custom_fake = xy_sensor_i2c_write_reg16_impl;
+    xy_sensor_i2c_read_fake.return_val = -1;
+    xy_sensor_i2c_write_fake.return_val = -1;
+    xy_sensor_i2c_read_reg_fake.return_val = -1;
+    xy_sensor_i2c_write_reg_fake.return_val = -1;
+    xy_sensor_spi_read_fake.return_val = -1;
+    xy_sensor_spi_write_fake.return_val = -1;
+    xy_sensor_check_device_id_fake.return_val = -1;
 }
 
 void setUp(void)
@@ -133,9 +101,7 @@ void setUp(void)
     memset(&last_bus, 0, sizeof(last_bus));
     last_write_reg = 0;
     last_write_value = 0xFFFF;
-    read16_count = 0;
-    write16_count = 0;
-    fake_tick = 1000;
+    reset_sensor_fakes();
 
     fake_regs[REG_VER] = 0x0011;
     fake_regs[REG_VCELL] = 0xC350;
@@ -164,6 +130,9 @@ void test_max17043_registers_default_i2c_bus(void)
     xy_fuel_gauge_t *fg = registered_max17043();
 
     TEST_ASSERT_NOT_NULL(fg);
+    TEST_ASSERT_EQUAL_UINT(1, xy_sensor_bus_config_i2c_fake.call_count);
+    TEST_ASSERT_EQUAL_PTR((void *)0x17043, xy_sensor_bus_config_i2c_fake.arg1_val);
+    TEST_ASSERT_EQUAL_UINT8(MAX17043_ADDR, xy_sensor_bus_config_i2c_fake.arg2_val);
     TEST_ASSERT_EQUAL(XY_SENSOR_BUS_I2C, last_bus.type);
     TEST_ASSERT_EQUAL_PTR((void *)0x17043, last_bus.bus_handle);
     TEST_ASSERT_EQUAL_UINT8(MAX17043_ADDR, last_bus.address);
@@ -176,9 +145,13 @@ void test_max17043_init_writes_default_config(void)
     fg->initialized = false;
     TEST_ASSERT_EQUAL(XY_FG_OK, xy_fuel_gauge_init(fg));
     TEST_ASSERT_TRUE(fg->initialized);
+    TEST_ASSERT_EQUAL_UINT(1, xy_sensor_i2c_read_reg16_fake.call_count);
+    TEST_ASSERT_EQUAL_UINT8(REG_VER, xy_sensor_i2c_read_reg16_fake.arg1_val);
+    TEST_ASSERT_EQUAL_UINT(1, xy_sensor_i2c_write_reg16_fake.call_count);
+    TEST_ASSERT_EQUAL_UINT8(REG_CONFIG, xy_sensor_i2c_write_reg16_fake.arg1_val);
+    TEST_ASSERT_EQUAL_UINT16(0, xy_sensor_i2c_write_reg16_fake.arg2_val);
     TEST_ASSERT_EQUAL_UINT8(REG_CONFIG, last_write_reg);
     TEST_ASSERT_EQUAL_UINT16(0, last_write_value);
-    TEST_ASSERT_EQUAL_INT(1, write16_count);
 }
 
 void test_max17043_fetch_and_channel_get(void)
@@ -189,8 +162,9 @@ void test_max17043_fetch_and_channel_get(void)
     fg->initialized = false;
     TEST_ASSERT_EQUAL(XY_FG_OK, xy_fuel_gauge_init(fg));
 
-    fake_tick = 17043;
+    xy_os_tick_get_fake.return_val = 17043;
     TEST_ASSERT_EQUAL(XY_FG_OK, xy_fuel_gauge_fetch(fg));
+    TEST_ASSERT_EQUAL_UINT(1, xy_os_tick_get_fake.call_count);
     TEST_ASSERT_EQUAL_UINT32(17043, fg->latest.timestamp);
 
     TEST_ASSERT_EQUAL(XY_FG_OK, xy_fuel_gauge_get(fg, XY_FG_DATA_VOLTAGE, &value));
@@ -202,7 +176,8 @@ void test_max17043_fetch_and_channel_get(void)
     TEST_ASSERT_EQUAL(XY_FG_OK, xy_fuel_gauge_get(fg, XY_FG_DATA_CURRENT, &value));
     TEST_ASSERT_EQUAL_INT32(-208, value);
 
-    TEST_ASSERT_GREATER_THAN(0, read16_count);
+    TEST_ASSERT_GREATER_OR_EQUAL_UINT(4, xy_sensor_i2c_read_reg16_fake.call_count);
+    TEST_ASSERT_EQUAL_UINT8(REG_CRATE, xy_sensor_i2c_read_reg16_fake.arg1_val);
 }
 
 void test_max17043_rejects_unsupported_channel(void)
