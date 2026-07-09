2a41f7f65e14a63877ea31095c46015650612fcb
ZeroZap <zerozap2020@gmail.com>
2026-07-09 10:38:53 +0800

    test(unit): introduce fff for gui core callbacks

--- tests/unit/gui/test_gui_core.c
+++ tests/unit/gui/test_gui_core.c
@@ -3,38 +3,44 @@
 #include <string.h>
 
 #include "unity.h"
+#include "fff.h"
 #include "xy_gui.h"
 
-static int g_init_count;
-static int g_flush_count;
-static int g_pixel_count;
-static int g_line_count;
-static int g_rect_count;
-static int g_fill_count;
-static int g_char_count;
 static int16_t g_last_x;
 static int16_t g_last_y;
 static int16_t g_last_w;
 static int16_t g_last_h;
 static uint16_t g_last_color;
 
+DEFINE_FFF_GLOBALS;
+
+FAKE_VALUE_FUNC(int, mock_init)
+FAKE_VALUE_FUNC(int, mock_draw_pixel, int16_t, int16_t, uint16_t)
+FAKE_VALUE_FUNC(int, mock_draw_line, int16_t, int16_t, int16_t, int16_t, uint16_t)
+FAKE_VALUE_FUNC(int, mock_draw_rect, int16_t, int16_t, int16_t, int16_t, uint16_t)
+FAKE_VALUE_FUNC(int, mock_fill_rect, int16_t, int16_t, int16_t, int16_t, uint16_t)
+FAKE_VALUE_FUNC(int, mock_draw_char, int16_t, int16_t, char, uint16_t)
+FAKE_VALUE_FUNC(int, mock_flush)
+
 void setUp(void)
 {
-}
+    RESET_FAKE(mock_init);
+    RESET_FAKE(mock_draw_pixel);
+    RESET_FAKE(mock_draw_line);
+    RESET_FAKE(mock_draw_rect);
+    RESET_FAKE(mock_fill_rect);
+    RESET_FAKE(mock_draw_char);
+    RESET_FAKE(mock_flush);
+    FFF_RESET_HISTORY();
 
-void tearDown(void)
-{
-}
+    mock_init_fake.return_val = XY_GUI_OK;
+    mock_draw_pixel_fake.return_val = XY_GUI_OK;
+    mock_draw_line_fake.return_val = XY_GUI_OK;
+    mock_draw_rect_fake.return_val = XY_GUI_OK;
+    mock_fill_rect_fake.return_val = XY_GUI_OK;
+    mock_draw_char_fake.return_val = XY_GUI_OK;
+    mock_flush_fake.return_val = XY_GUI_OK;
 
-static void reset_mock(void)
-{
-    g_init_count = 0;
-    g_flush_count = 0;
-    g_pixel_count = 0;
-    g_line_count = 0;
-    g_rect_count = 0;
-    g_fill_count = 0;
-    g_char_count = 0;
     g_last_x = 0;
     g_last_y = 0;
     g_last_w = 0;
@@ -42,68 +48,8 @@ static void reset_mock(void)
     g_last_color = 0;
 }
 
-static int mock_init(void)
+void tearDown(void)
 {
-    g_init_count++;
-    return XY_GUI_OK;
-}
-
-static int mock_draw_pixel(int16_t x, int16_t y, uint16_t color)
-{
-    g_pixel_count++;
-    g_last_x = x;
-    g_last_y = y;
-    g_last_color = color;
-    return XY_GUI_OK;
-}
-
-static int mock_draw_line(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t color)
-{
-    (void)x2;
-    (void)y2;
-    g_line_count++;
-    g_last_x = x1;
-    g_last_y = y1;
-    g_last_color = color;
-    return XY_GUI_OK;
-}
-
-static int mock_draw_rect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color)
-{
-    g_rect_count++;
-    g_last_x = x;
-    g_last_y = y;
-    g_last_w = w;
-    g_last_h = h;
-    g_last_color = color;
-    return XY_GUI_OK;
-}
-
-static int mock_fill_rect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color)
-{
-    g_fill_count++;
-    g_last_x = x;
-    g_last_y = y;
-    g_last_w = w;
-    g_last_h = h;
-    g_last_color = color;
-    return XY_GUI_OK;
-}
-
-static int mock_draw_char(int16_t x, int16_t y, char c, uint16_t color)
-{
-    (void)c;
-    g_char_count++;
-    g_last_x = x;
-    g_last_y = y;
-    g_last_color = color;
-    return XY_GUI_OK;
-}
-
-static int mock_flush(void)
-{
-    g_flush_count++;
-    return XY_GUI_OK;
 }
 
 static xy_gui_disp_drv_t mock_driver(void)
@@ -120,35 +66,68 @@ static xy_gui_disp_drv_t mock_driver(void)
     return drv;
 }
 
+static void capture_pixel_from_fake(void)
+{
+    g_last_x = mock_draw_pixel_fake.arg0_val;
+    g_last_y = mock_draw_pixel_fake.arg1_val;
+    g_last_color = mock_draw_pixel_fake.arg2_val;
+}
+
+static void capture_fill_rect_from_fake(void)
+{
+    g_last_x = mock_fill_rect_fake.arg0_val;
+    g_last_y = mock_fill_rect_fake.arg1_val;
+    g_last_w = mock_fill_rect_fake.arg2_val;
+    g_last_h = mock_fill_rect_fake.arg3_val;
+    g_last_color = mock_fill_rect_fake.arg4_val;
+}
+
+static void capture_char_from_history(unsigned int history_index)
+{
+    g_last_x = mock_draw_pixel_fake.arg0_history[history_index];
+    g_last_y = mock_draw_pixel_fake.arg1_history[history_index];
+    g_last_color = mock_draw_pixel_fake.arg2_history[history_index];
+}
+
 static void test_lifecycle_clear_flush_and_bounds(void)
 {
     xy_gui_t gui;
     xy_gui_disp_drv_t drv = mock_driver();
 
-    reset_mock();
     TEST_ASSERT_EQUAL_INT(XY_GUI_INVALID_PARAM, xy_gui_init(NULL, 64, 32, &drv));
     TEST_ASSERT_EQUAL_INT(XY_GUI_INVALID_PARAM, xy_gui_init(&gui, 64, 32, NULL));
     TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_init(&gui, 64, 32, &drv));
     TEST_ASSERT_TRUE(gui.initialized);
     TEST_ASSERT_EQUAL_INT16(64, gui.width);
     TEST_ASSERT_EQUAL_INT16(32, gui.height);
-    TEST_ASSERT_EQUAL_INT(1, g_init_count);
+    TEST_ASSERT_EQUAL_UINT(1U, mock_init_fake.call_count);
 
     TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_clear(&gui, GUI_COLOR_BLUE));
     TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_BLUE, gui.bg_color);
-    TEST_ASSERT_EQUAL_INT(1, g_fill_count);
+    TEST_ASSERT_EQUAL_UINT(1U, mock_fill_rect_fake.call_count);
+    TEST_ASSERT_EQUAL_INT16(0, mock_fill_rect_fake.arg0_val);
+    TEST_ASSERT_EQUAL_INT16(0, mock_fill_rect_fake.arg1_val);
+    TEST_ASSERT_EQUAL_INT16(64, mock_fill_rect_fake.arg2_val);
+    TEST_ASSERT_EQUAL_INT16(32, mock_fill_rect_fake.arg3_val);
+    TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_BLUE, mock_fill_rect_fake.arg4_val);
+    capture_fill_rect_from_fake();
     TEST_ASSERT_EQUAL_INT16(64, g_last_w);
     TEST_ASSERT_EQUAL_INT16(32, g_last_h);
 
     TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_draw_pixel(&gui, 63, 31, GUI_COLOR_RED));
-    TEST_ASSERT_EQUAL_INT(1, g_pixel_count);
+    TEST_ASSERT_EQUAL_UINT(1U, mock_draw_pixel_fake.call_count);
+    TEST_ASSERT_EQUAL_INT16(63, mock_draw_pixel_fake.arg0_val);
+    TEST_ASSERT_EQUAL_INT16(31, mock_draw_pixel_fake.arg1_val);
+    TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_RED, mock_draw_pixel_fake.arg2_val);
+    capture_pixel_from_fake();
     TEST_ASSERT_EQUAL_INT16(63, g_last_x);
     TEST_ASSERT_EQUAL_INT16(31, g_last_y);
     TEST_ASSERT_EQUAL_INT(XY_GUI_INVALID_PARAM, xy_gui_draw_pixel(&gui, 64, 0, GUI_COLOR_RED));
     TEST_ASSERT_EQUAL_INT(XY_GUI_INVALID_PARAM, xy_gui_draw_pixel(&gui, -1, 0, GUI_COLOR_RED));
+    TEST_ASSERT_EQUAL_UINT(1U, mock_draw_pixel_fake.call_count);
 
     TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_flush(&gui));
-    TEST_ASSERT_EQUAL_INT(1, g_flush_count);
+    TEST_ASSERT_EQUAL_UINT(1U, mock_flush_fake.call_count);
     TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_deinit(&gui));
     TEST_ASSERT_FALSE(gui.initialized);
     TEST_ASSERT_EQUAL_INT(XY_GUI_INVALID_PARAM, xy_gui_clear(&gui, GUI_COLOR_BLACK));
@@ -158,22 +137,44 @@ static void test_drawing_and_string_callbacks(void)
 {
     xy_gui_t gui;
     xy_gui_disp_drv_t drv = mock_driver();
+    unsigned int pixel_count_after_line;
+    unsigned int pixel_count_after_rect;
+    unsigned int pixel_count_after_char;
 
-    reset_mock();
     TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_init(&gui, 64, 32, &drv));
     TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_draw_line(&gui, 0, 0, 3, 0, GUI_COLOR_GREEN));
-    TEST_ASSERT_EQUAL_INT(4, g_pixel_count);
+    TEST_ASSERT_EQUAL_UINT(4U, mock_draw_pixel_fake.call_count);
+    TEST_ASSERT_EQUAL_INT16(0, mock_draw_pixel_fake.arg0_history[0]);
+    TEST_ASSERT_EQUAL_INT16(3, mock_draw_pixel_fake.arg0_val);
+    TEST_ASSERT_EQUAL_INT16(0, mock_draw_pixel_fake.arg1_val);
+    TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_GREEN, mock_draw_pixel_fake.arg2_val);
+    pixel_count_after_line = mock_draw_pixel_fake.call_count;
+
     TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_draw_rect(&gui, 1, 2, 4, 5, GUI_COLOR_WHITE));
-    TEST_ASSERT_GREATER_THAN_INT(4, g_pixel_count);
+    TEST_ASSERT_GREATER_THAN_UINT(pixel_count_after_line, mock_draw_pixel_fake.call_count);
+    pixel_count_after_rect = mock_draw_pixel_fake.call_count;
+
     TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_fill_rect(&gui, 2, 3, 5, 6, GUI_COLOR_CYAN));
-    TEST_ASSERT_EQUAL_INT(1, g_fill_count);
+    TEST_ASSERT_EQUAL_UINT(1U, mock_fill_rect_fake.call_count);
+    TEST_ASSERT_EQUAL_INT16(2, mock_fill_rect_fake.arg0_val);
+    TEST_ASSERT_EQUAL_INT16(3, mock_fill_rect_fake.arg1_val);
+    TEST_ASSERT_EQUAL_INT16(5, mock_fill_rect_fake.arg2_val);
+    TEST_ASSERT_EQUAL_INT16(6, mock_fill_rect_fake.arg3_val);
+    TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_CYAN, mock_fill_rect_fake.arg4_val);
+    capture_fill_rect_from_fake();
     TEST_ASSERT_EQUAL_INT16(2, g_last_x);
     TEST_ASSERT_EQUAL_INT16(3, g_last_y);
     TEST_ASSERT_EQUAL_INT16(5, g_last_w);
     TEST_ASSERT_EQUAL_INT16(6, g_last_h);
-    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_draw_char(&gui, 0, 0, 'A', GUI_COLOR_WHITE));
-    TEST_ASSERT_GREATER_THAN_INT(4, g_pixel_count);
-    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_draw_string(&gui, 0, 0, "Hi", GUI_COLOR_WHITE));
+
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_draw_char(&gui, 0, 0, '0', GUI_COLOR_WHITE));
+    TEST_ASSERT_GREATER_THAN_UINT(pixel_count_after_rect, mock_draw_pixel_fake.call_count);
+    pixel_count_after_char = mock_draw_pixel_fake.call_count;
+    capture_char_from_history(pixel_count_after_char - 1U);
+    TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_WHITE, g_last_color);
+
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_draw_string(&gui, 0, 0, "09", GUI_COLOR_WHITE));
+    TEST_ASSERT_GREATER_THAN_UINT(pixel_count_after_char, mock_draw_pixel_fake.call_count);
     TEST_ASSERT_EQUAL_INT(XY_GUI_INVALID_PARAM, xy_gui_draw_string(&gui, 0, 0, NULL, GUI_COLOR_WHITE));
 }
 
@@ -185,8 +186,8 @@ static void test_object_lifecycle_properties_and_redraw(void)
     xy_gui_obj_t *label;
     xy_gui_obj_t *button;
     xy_gui_obj_t external = {0};
+    unsigned int pixel_count_after_label_text;
 
-    reset_mock();
     TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_init(&gui, 80, 40, &drv));
     label = xy_gui_obj_create(&gui, XY_GUI_OBJ_LABEL, &rect);
     TEST_ASSERT_NOT_NULL(label);
@@ -196,12 +197,16 @@ static void test_object_lifecycle_properties_and_redraw(void)
     TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_BLACK, label->fg_color.color);
     TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_WHITE, label->bg_color.color);
 
-    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_obj_set_text(&gui, label, "OK"));
+    TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_obj_set_text(&gui, label, "09"));
     TEST_ASSERT_NOT_NULL(label->text);
-    TEST_ASSERT_EQUAL_STRING("OK", label->text);
+    TEST_ASSERT_EQUAL_STRING("09", label->text);
+    TEST_ASSERT_GREATER_THAN_UINT(0U, mock_draw_pixel_fake.call_count);
+    pixel_count_after_label_text = mock_draw_pixel_fake.call_count;
     TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_obj_set_color(&gui, label, GUI_COLOR_RED, GUI_COLOR_BLUE));
     TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_RED, label->fg_color.color);
     TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_BLUE, label->bg_color.color);
+    TEST_ASSERT_GREATER_THAN_UINT(pixel_count_after_label_text, mock_draw_pixel_fake.call_count);
+    TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_RED, mock_draw_pixel_fake.arg2_val);
 
     TEST_ASSERT_EQUAL_INT(XY_GUI_INVALID_PARAM, xy_gui_obj_set_visible(&gui, label, false));
     TEST_ASSERT_FALSE(label->visible);
@@ -213,7 +218,12 @@ static void test_object_lifecycle_properties_and_redraw(void)
     TEST_ASSERT_NOT_NULL(button);
     TEST_ASSERT_EQUAL_INT(2, gui.obj_count);
     TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_obj_set_text(&gui, button, "B"));
-    TEST_ASSERT_GREATER_OR_EQUAL_INT(1, g_fill_count);
+    TEST_ASSERT_GREATER_OR_EQUAL_UINT(1U, mock_fill_rect_fake.call_count);
+    TEST_ASSERT_EQUAL_INT16(rect.x, mock_fill_rect_fake.arg0_val);
+    TEST_ASSERT_EQUAL_INT16(rect.y, mock_fill_rect_fake.arg1_val);
+    TEST_ASSERT_EQUAL_INT16(rect.width, mock_fill_rect_fake.arg2_val);
+    TEST_ASSERT_EQUAL_INT16(rect.height, mock_fill_rect_fake.arg3_val);
+    TEST_ASSERT_EQUAL_HEX16(GUI_COLOR_WHITE, mock_fill_rect_fake.arg4_val);
 
     TEST_ASSERT_EQUAL_INT(XY_GUI_NOT_FOUND, xy_gui_obj_delete(&gui, &external));
     TEST_ASSERT_EQUAL_INT(XY_GUI_OK, xy_gui_obj_delete(&gui, label));
