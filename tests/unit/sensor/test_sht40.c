e3a98ad922816d5246da35b312a5ffedbc6531f6
ZeroZap <zerozap2020@gmail.com>
2026-07-12 21:38:03 +0800

    test(sensor): expand sht40 host coverage

--- tests/unit/sensor/test_sht40.c
+++ tests/unit/sensor/test_sht40.c
@@ -13,7 +13,9 @@ static int g_read_ret_queue[16];
 static size_t g_read_count;
 static size_t g_read_index;
 static uint8_t g_write_queue[8];
+static int g_write_ret_queue[8];
 static size_t g_write_count;
+static size_t g_write_index;
 static uint32_t g_tick;
 static uint32_t g_delay_total;
 static uint8_t g_last_addr;
@@ -66,9 +68,10 @@ xy_error_t xy_i2c_device_write(xy_i2c_device_t *dev, const uint8_t *data, size_t
     TEST_ASSERT_NOT_NULL(data);
     TEST_ASSERT_EQUAL_UINT(1U, len);
     TEST_ASSERT_LESS_THAN_UINT(sizeof(g_write_queue), g_write_count);
+    TEST_ASSERT_LESS_THAN_UINT(sizeof(g_write_ret_queue) / sizeof(g_write_ret_queue[0]), g_write_index);
 
     g_write_queue[g_write_count++] = data[0];
-    return XY_DEVICE_OK;
+    return g_write_ret_queue[g_write_index++];
 }
 
 uint32_t xy_os_tick_get(void)
@@ -121,9 +124,13 @@ void setUp(void)
     memset(g_read_len_queue, 0, sizeof(g_read_len_queue));
     memset(g_read_ret_queue, 0, sizeof(g_read_ret_queue));
     memset(g_write_queue, 0, sizeof(g_write_queue));
+    for (size_t i = 0; i < sizeof(g_write_ret_queue) / sizeof(g_write_ret_queue[0]); ++i) {
+        g_write_ret_queue[i] = XY_DEVICE_OK;
+    }
     g_read_count = 0;
     g_read_index = 0;
     g_write_count = 0;
+    g_write_index = 0;
     g_tick = 2000;
     g_delay_total = 0;
     g_last_addr = 0;
@@ -206,6 +213,67 @@ static void test_read_bad_crc_does_not_overwrite_cached_measurement(void)
     TEST_ASSERT_EQUAL_UINT16(5650U, dev.data.humidity);
 }
 
+static void test_init_reports_i2c_write_and_read_failures(void)
+{
+    xy_sht40_t dev;
+    int fake_bus;
+
+    g_write_ret_queue[0] = XY_DEVICE_ERROR;
+    TEST_ASSERT_EQUAL_INT(XY_SHT40_NOT_FOUND, xy_sht40_init(&dev, &fake_bus));
+    TEST_ASSERT_FALSE(dev.initialized);
+
+    setUp();
+    queue_read(NULL, 6U, XY_DEVICE_ERROR);
+    TEST_ASSERT_EQUAL_INT(XY_DEVICE_ERROR, xy_sht40_init(&dev, &fake_bus));
+    TEST_ASSERT_FALSE(dev.initialized);
+}
+
+static void test_deinit_rejects_null_and_clears_initialized_flag(void)
+{
+    xy_sht40_t dev;
+    int fake_bus;
+
+    TEST_ASSERT_EQUAL_INT(XY_SHT40_INVALID_PARAM, xy_sht40_deinit(NULL));
+
+    queue_pair_payload(0x1234U, 0xABCDU);
+    TEST_ASSERT_EQUAL_INT(XY_SHT40_OK, xy_sht40_init(&dev, &fake_bus));
+    TEST_ASSERT_TRUE(dev.initialized);
+    TEST_ASSERT_EQUAL_INT(XY_SHT40_OK, xy_sht40_deinit(&dev));
+    TEST_ASSERT_FALSE(dev.initialized);
+}
+
+static void test_read_rejects_invalid_or_uninitialized_and_propagates_write_failure(void)
+{
+    xy_sht40_t dev;
+    int fake_bus;
+
+    TEST_ASSERT_EQUAL_INT(XY_SHT40_INVALID_PARAM, xy_sht40_read(NULL));
+    memset(&dev, 0, sizeof(dev));
+    TEST_ASSERT_EQUAL_INT(XY_SHT40_INVALID_PARAM, xy_sht40_read(&dev));
+
+    queue_pair_payload(0x1234U, 0xABCDU);
+    TEST_ASSERT_EQUAL_INT(XY_SHT40_OK, xy_sht40_init(&dev, &fake_bus));
+    g_write_ret_queue[g_write_index] = XY_DEVICE_ERROR;
+    TEST_ASSERT_EQUAL_INT(XY_DEVICE_ERROR, xy_sht40_read(&dev));
+}
+
+static void test_low_precision_read_uses_low_power_command_and_delay(void)
+{
+    xy_sht40_t dev;
+    int fake_bus;
+
+    queue_pair_payload(0x1234U, 0xABCDU);
+    TEST_ASSERT_EQUAL_INT(XY_SHT40_OK, xy_sht40_init(&dev, &fake_bus));
+    TEST_ASSERT_EQUAL_INT(XY_SHT40_OK, xy_sht40_set_precision(&dev, XY_SHT40_LOW_PRECISION));
+
+    queue_pair_payload(0xFFFFU, 0x0000U);
+    TEST_ASSERT_EQUAL_INT(XY_SHT40_OK, xy_sht40_read(&dev));
+    TEST_ASSERT_EQUAL_UINT8(SHT40_CMD_MEASURE_LPM, g_write_queue[1]);
+    TEST_ASSERT_EQUAL_UINT32(20U, g_delay_total); /* 10ms serial + 10ms low precision */
+    TEST_ASSERT_INT16_WITHIN(1, 12999, dev.data.temperature);
+    TEST_ASSERT_EQUAL_UINT16(64936U, dev.data.humidity); /* unsigned wrap documents current behavior */
+}
+
 static void test_getters_and_precision_validate_inputs(void)
 {
     xy_sht40_t dev;
@@ -240,8 +308,12 @@ int main(void)
     UNITY_BEGIN();
     RUN_TEST(test_init_rejects_invalid_inputs_and_reads_serial);
     RUN_TEST(test_init_rejects_bad_serial_crc_without_initializing);
+    RUN_TEST(test_init_reports_i2c_write_and_read_failures);
     RUN_TEST(test_read_uses_precision_command_and_converts_measurement);
     RUN_TEST(test_read_bad_crc_does_not_overwrite_cached_measurement);
+    RUN_TEST(test_deinit_rejects_null_and_clears_initialized_flag);
+    RUN_TEST(test_read_rejects_invalid_or_uninitialized_and_propagates_write_failure);
+    RUN_TEST(test_low_precision_read_uses_low_power_command_and_delay);
     RUN_TEST(test_getters_and_precision_validate_inputs);
     return UNITY_END();
 }
