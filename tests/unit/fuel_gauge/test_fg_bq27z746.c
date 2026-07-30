29dd814d21eb77c79aadf029682c74ffb0eced62
ZeroZap <zerozap2020@gmail.com>
2026-07-30 14:59:56 +0800

    test: harden bq27z746 inline getter guards

--- tests/unit/fuel_gauge/test_fg_bq27z746.c
+++ tests/unit/fuel_gauge/test_fg_bq27z746.c
@@ -472,6 +472,22 @@ void test_bq27z746_inline_getters_preserve_outputs_on_fetch_failure(void)
     fg->initialized = false;
     TEST_ASSERT_EQUAL(XY_FG_OK, xy_fuel_gauge_init(fg));
 
+    TEST_ASSERT_EQUAL(XY_FG_ERROR_INVALID_PARAM, xy_fuel_gauge_get_voltage(NULL, &voltage));
+    TEST_ASSERT_EQUAL_UINT16(0x0746, voltage);
+    TEST_ASSERT_EQUAL(XY_FG_ERROR_INVALID_PARAM, xy_fuel_gauge_get_voltage(fg, NULL));
+    TEST_ASSERT_EQUAL(XY_FG_ERROR_INVALID_PARAM, xy_fuel_gauge_get_current(NULL, &current));
+    TEST_ASSERT_EQUAL_INT16(-746, current);
+    TEST_ASSERT_EQUAL(XY_FG_ERROR_INVALID_PARAM, xy_fuel_gauge_get_current(fg, NULL));
+    TEST_ASSERT_EQUAL(XY_FG_ERROR_INVALID_PARAM, xy_fuel_gauge_get_soc(NULL, &soc));
+    TEST_ASSERT_EQUAL_UINT8(74, soc);
+    TEST_ASSERT_EQUAL(XY_FG_ERROR_INVALID_PARAM, xy_fuel_gauge_get_soc(fg, NULL));
+    TEST_ASSERT_EQUAL(XY_FG_ERROR_INVALID_PARAM, xy_fuel_gauge_get_soh(NULL, &soh));
+    TEST_ASSERT_EQUAL_UINT8(46, soh);
+    TEST_ASSERT_EQUAL(XY_FG_ERROR_INVALID_PARAM, xy_fuel_gauge_get_soh(fg, NULL));
+    TEST_ASSERT_EQUAL(XY_FG_ERROR_INVALID_PARAM, xy_fuel_gauge_get_temperature(NULL, &temp));
+    TEST_ASSERT_EQUAL_INT16(-27, temp);
+    TEST_ASSERT_EQUAL(XY_FG_ERROR_INVALID_PARAM, xy_fuel_gauge_get_temperature(fg, NULL));
+
     fake_fail_reads(REG_VOLT, 3);
     TEST_ASSERT_EQUAL(XY_FG_ERROR, xy_fuel_gauge_get_voltage(fg, &voltage));
     TEST_ASSERT_EQUAL_UINT16(0x0746, voltage);
