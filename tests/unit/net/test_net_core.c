1600d0f5f3e614dfcb1cee216c6b51db7ff9effb
ZeroZap <zerozap2020@gmail.com>
2026-08-05 12:32:07 +0800

    test: cover disabled net protocol headers

--- tests/unit/net/test_net_core.c
+++ tests/unit/net/test_net_core.c
@@ -5,6 +5,9 @@
 
 #include "xy_net.h"
 
+#include "xy_can.h"
+#include "xy_lte.h"
+
 #include "unity.h"
 
 #include <stdint.h>
@@ -59,6 +62,56 @@ static void test_net_protocol_feature_flags_are_public(void)
     TEST_ASSERT_EQUAL_INT(0, XY_NET_ENABLE_LTE);
 }
 
+static void test_disabled_protocol_headers_remain_direct_opt_in_contracts(void)
+{
+    xy_can_config_t can_config = {
+        .baudrate = 500000U,
+        .rx_fifo_size = 4U,
+        .tx_fifo_size = 4U,
+    };
+    xy_can_msg_t can_msg = {
+        .id = 0x123U,
+        .len = 2U,
+        .data = {0xAAU, 0x55U},
+    };
+    xy_lte_t lte = {
+        .baudrate = 115200U,
+        .net_type = XY_LTE_NET_4G,
+    };
+    xy_lte_signal_t signal = {
+        .rssi = -70,
+        .ber = 1,
+        .rsrp = -95,
+        .rsrq = -10,
+        .sinr = 20,
+    };
+    xy_lte_pdp_context_t pdp = {
+        .cid = 1U,
+        .apn = "internet",
+    };
+
+    TEST_ASSERT_EQUAL_INT(0, XY_NET_ENABLE_CAN);
+    TEST_ASSERT_EQUAL_UINT32(500000U, can_config.baudrate);
+    TEST_ASSERT_EQUAL_UINT32(4U, can_config.rx_fifo_size);
+    TEST_ASSERT_EQUAL_UINT32(4U, can_config.tx_fifo_size);
+    TEST_ASSERT_EQUAL_UINT32(0x123U, can_msg.id);
+    TEST_ASSERT_EQUAL_UINT8(2U, can_msg.len);
+    TEST_ASSERT_EQUAL_HEX8(0xAAU, can_msg.data[0]);
+    TEST_ASSERT_EQUAL_HEX8(0x55U, can_msg.data[1]);
+    TEST_ASSERT_EQUAL_INT(XY_CAN_OK, 0);
+    TEST_ASSERT_EQUAL_INT(XY_CAN_INVALID_PARAM, -2);
+
+    TEST_ASSERT_EQUAL_INT(0, XY_NET_ENABLE_LTE);
+    TEST_ASSERT_EQUAL_UINT32(115200U, lte.baudrate);
+    TEST_ASSERT_EQUAL_INT(XY_LTE_NET_4G, lte.net_type);
+    TEST_ASSERT_EQUAL_INT(-70, signal.rssi);
+    TEST_ASSERT_EQUAL_INT(20, signal.sinr);
+    TEST_ASSERT_EQUAL_UINT8(1U, pdp.cid);
+    TEST_ASSERT_EQUAL_STRING("internet", pdp.apn);
+    TEST_ASSERT_EQUAL_INT(XY_LTE_OK, 0);
+    TEST_ASSERT_EQUAL_INT(XY_LTE_INVALID_PARAM, -2);
+}
+
 static void test_net_umbrella_exports_modbus_compat(void)
 {
     mb_slave_t slave;
@@ -108,6 +161,7 @@ int main(void)
     RUN_TEST(test_net_platform_helpers);
     RUN_TEST(test_net_config_default_allocators_are_publicly_usable);
     RUN_TEST(test_net_protocol_feature_flags_are_public);
+    RUN_TEST(test_disabled_protocol_headers_remain_direct_opt_in_contracts);
     RUN_TEST(test_net_umbrella_exports_modbus_compat);
     RUN_TEST(test_net_umbrella_exports_at_client_contract);
     return UNITY_END();
